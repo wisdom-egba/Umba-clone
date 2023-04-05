f6d7a3685f4407592e15a166c832d341a756257b
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2023-04-05 04:21:29 +0100

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,73 +1,83 @@
 import React from "react"
 import umba from "../assets/logo2.png"
+import { AiOutlineTwitter } from "react-icons/ai"
+import { BsFacebook, BsLinkedin } from "react-icons/bs"
+import { FaInstagram } from "react-icons/fa"
 const Footer = () => {
   return (
-    <div className="w-full bg-white py-16 md:py-24">
-      <div className="max-w-[370px] md:max-w-[1480px] m-auto grid md:grid-cols-5 max-[780px]:grid-col-6 py-10 md:py-10">
-        <div>
-          <img className="w-[50%]" src={umba} alt="" />
+    <div className=" mt-[180px] w-full bg-[#e0eaf6] md:py-16">
+      <div className=" ml-5 max-w-[1370px] md:max-w-[77%] lg:max-w-[55%] m-auto grid md:grid-cols-5 grid-col-6 pt-12  ">
+        <div className="flex flex-col mr-16 ">
+          <img className="w-[25%] md:w-[50%]" src={umba} alt="" />
         </div>
-        <div>
-          <h1>Company</h1>
+        <div className="mr-16 mt-4">
+          <h1 className="font-bold mb-3 md:mb-6">Company</h1>
           <ul>
-            <li>
+            <li className="mb-2 md:mb-3">
               <a href="#"></a>Home
             </li>
-            <li>
+            <li className="mb-2 md:mb-3">
               <a href="#"></a>Careers
             </li>
-            <li>
+            <li className="mb-2 md:mb-3">
               <a href="#"></a>Contact Us
             </li>
           </ul>
         </div>
-        <div>
-          <h1>Location</h1>
+        <div className=" mr-[70px] mt-5">
+          <h1 className="font-bold mb-3 md:mb-6">Locations</h1>
           <ul>
-            <li>
+            <li className="md:mb-3 mb-2">
               <a href="#"></a>Nigeria
             </li>
-            <li>
+            <li className="md:mb-3 mb-2">
               <a href="#"></a>Kenya
             </li>
           </ul>
         </div>
-        <div>
-          <h1>Company</h1>
+        <div className="w-[100%] mt-5">
+          <h1 className="font-bold mb-3 md:mb-6">Transparency</h1>
           <ul>
-            <li>
+            <li className="mb-3">
               <a href="#"></a>Privacy Policy
             </li>
-            <li>
+            <li className="mb-3">
               <a href="#"></a>General Terms of Service
             </li>
           </ul>
         </div>
-        <div>
-          <h1>Socials</h1>
+        <div className="md:ml-[45px] mt-5">
+          <h1 className="font-bold mb-3 md:mb-6">Socials</h1>
           <ul>
-            <div>
-              <li>
-                <a href="">Twitter</a>
-              </li>
+            <div className="mb-3">
+              <a className="flex gap-3  items-center" href="">
+                <AiOutlineTwitter size={20} />
+                Twitter
+              </a>
             </div>
-            <div>
-              <li>
-                <a href="">Facebook</a>
-              </li>
+            <div className="mb-3">
+              <a className="flex gap-3  items-center" href="">
+                <BsFacebook size={20} />
+                Facebook
+              </a>
             </div>
-            <div>
-              <li>
-                <a href="">Instagram</a>
-              </li>
+            <div className="mb-3">
+              <a className="flex gap-3 items-center" href="">
+                <FaInstagram size={20} />
+                Instagram
+              </a>
             </div>
-            <div>
-              <li>
-                <a href="">LinkedIn</a>
-              </li>
+            <div className="mb-3">
+              <a className="flex gap-3 items-center" href="">
+                <BsLinkedin size={20} />
+                LinkedIn
+              </a>
             </div>
           </ul>
         </div>
+      </div>
+      <div className="flex justify-center items-center pt-20 pb-2">
+        Copyright @ 2023 Umba. All rights reserved
       </div>
     </div>
   )
--- src/components/Open.jsx
+++ src/components/Open.jsx
@@ -3,31 +3,61 @@ import playstore from "../assets/playstore.png"
 import appstore from "../assets/applestore.png"
 const Open = () => {
   return (
-    <div className="w-full  pt-[10px] md:pt-[10px] lg:pt-[0px] grid grid-cols-2">
-      <div></div>
-      <div className="bg-[#e0eaf6] w-[100%] h-screen flex justify-end items-end"></div>
-      <div className="absolute h-full bg-[#e0eaf6] max-w-[340px] md:max-w-[95%] lg:max-w-[65%] m-auto py-10 flex rounded-l-[80px] rounded-r-[80px] justify-end items-center ">
-        <div className=" h-full flex w-[80%] border border-l-2 border-l-black my-6">
-          <div className="ml-9">
-            <h1 className="text-[24px] leading-tight md:text-7xl lg:text-[90px] lg:w-[80%] md:text-[44px] py-2 w-[100%] lg:w-[100%]">
+    <div className="pt-[60px] grid grid-cols-2">
+      <div className=" bg-white h-[400px] md:h-[430px] lg:h-[800px]"></div>
+      <div className=" bg-[#e0eaf6] h-[400px] md:h-[430px] lg:h-[800px]"></div>
+      <div className=" bg-[#e0eaf6] h-[400px] md:h-[430px] lg:h-[800px] rounded-l-[90px] rounded-r-[90px] flex justify-center absolute right-0 w-[94%] md:w-[97%] lg:w-[84%] ml-[90px]">
+        <div className=" md:ml-[120px] w-[85%] md:w-[80%] border border-l-2 border-l-black my-[90px] md:my-[97px] lg:my-[123px] ">
+          <div className="ml-9 md:ml-8 flex flex-col justify-center  ">
+            <h1 className=" leading-tight  text-[28px] leading-tight md:text-7xl lg:text-[90px] w-[100%] lg:w-[80%] md:text-[44px] py-2 md:w-[100%] lg:w-[60%] ">
               Open your account today and experience next-gen banking
             </h1>
-            <p className="leading-tight font-medium text-[15px] md:text-lg w-[100%] md:w-[80%] lg:w-[52%] lg:py-6 text-[#636666] leading-tight">
+            <p className="leading-tight font-medium text-[15px] md:text-lg w-[100%] md:w-[80%] lg:w-[38%] lg:py-4 text-[#636666] leading-tight">
               With an Umba account, you have everything you need from your bank.
               Spend, pay bills, get loans, and cashback all from your phone.
             </p>
-            <div className="flex items-center py-6 lg:py-2 gap-4 md:gap-0">
-              <a href="#">
-                <img className="md:w-[65%]" src={playstore} alt="" />
-              </a>
-              <a href="#">
-                <img className="md:w-[65%]" src={appstore} alt="" />
-              </a>
+            <div className="flex py-6 gap-4 lg:py-2 ">
+              <div>
+                <a href="#">
+                  <img className="md:w-[62%]" src={playstore} alt="" />
+                </a>
+              </div>
+              <div>
+                <a href="#">
+                  <img className="md:w-[62%]" src={appstore} alt="" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+
+    // <div className="w-full pt-[10px] md:pt-[10px] lg:pt-[0px] grid grid-cols-2">
+    //   <div></div>
+    //   <div className="bg-[#e0eaf6] flex justify-end items-end"></div>
+    //   <div className="absolute  bg-[#e0eaf6] max-w-[340px] md:max-w-[95%] lg:max-w-[65%] m-auto py-10 flex rounded-l-[80px] rounded-r-[80px] justify-end items-center ">
+    //     <div className=" h-full flex w-[80%] border border-l-2 border-l-black my-6">
+    //       <div className="ml-9">
+    //         <h1 className="text-[24px] leading-tight md:text-7xl lg:text-[90px] lg:w-[80%] md:text-[44px] py-2 w-[100%] lg:w-[100%]">
+    //           Open your account today and experience next-gen banking
+    //         </h1>
+    //         <p className="leading-tight font-medium text-[15px] md:text-lg w-[100%] md:w-[80%] lg:w-[52%] lg:py-6 text-[#636666] leading-tight">
+    //           With an Umba account, you have everything you need from your bank.
+    //           Spend, pay bills, get loans, and cashback all from your phone.
+    //         </p>
+    //         <div className="flex items-center py-6 lg:py-2 gap-4 md:gap-0">
+    //           <a href="#">
+    //             <img className="md:w-[65%]" src={playstore} alt="" />
+    //           </a>
+    //           <a href="#">
+    //             <img className="md:w-[65%]" src={appstore} alt="" />
+    //           </a>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
