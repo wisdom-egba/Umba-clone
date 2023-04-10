8f25259b02a286953c2d09a03fe043aa0b447978
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2023-04-10 22:56:23 +0100

    updated

--- src/pages/Business/BisInfo.jsx
+++ src/pages/Business/BisInfo.jsx
@@ -1,7 +1,50 @@
 import React from "react"
+import laptop from "../../assets/laptop.png"
+import phone4 from "../../assets/phone4.png"
 
 const BisInfo = () => {
-  return <div></div>
+  return (
+    <div className="w-full bg-[white]   pt-[10px] md:pt-[10px] lg:pt-[0px]">
+      <div className="max-w-[100%] bg-[#e0eaf6] md:max-w-[100%] lg:max-w-[70%] m-auto  grid rounded-br-[200px] pb-[230px]">
+        <div className="md:flex m-auto justify-center max-w-[340px] md:max-w-[82%] lg:max-w-[70%]">
+          <div className="w-[80%]">
+            <h1 className="text-7xl">Maximizing work efficiency, every day!</h1>
+          </div>
+          <div>
+            <form action="" className="">
+              <input
+                className="rounded-[40px] w-full p-2 border border-transparent focus:border-white"
+                type="text"
+                placeholder="Name"
+                id="name"
+              />
+              <div className="flex gap-5 py-8">
+                <input
+                  className="rounded-[40px] p-2 w-[80%]"
+                  type="email"
+                  placeholder="Work mail"
+                />
+                <input
+                  className="rounded-[40px] p-2 w-[80%]"
+                  type="number"
+                  placeholder="Phone no."
+                />
+              </div>
+              <button className="bg-[#0d3356] m-auto px-7 py-2 text-white font-semibold text-lg ">
+                REQUEST DEMO
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="relative z-20 max-w-[340px] md:max-w-[82%] lg:max-w-[70%] m-auto ">
+        <img className="w-[92%]" src={laptop} alt="" />
+        <div className="absolute top-0 z-30 right-[0]">
+          <img className="w-[40%]" src={phone4} alt="" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default BisInfo
--- src/pages/Business/Business.jsx
+++ src/pages/Business/Business.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Footer from "../../components/Footer"
 
 import BisHeader from "./BisHeader"
+import BisInfo from "./BisInfo"
 import BisNavBar from "./BisNavBar"
 
 const Business = () => {
@@ -9,6 +10,7 @@ const Business = () => {
     <div>
       <BisHeader />
       <BisNavBar />
+      <BisInfo />
       <Footer />
     </div>
   )
