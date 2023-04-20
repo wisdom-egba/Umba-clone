70d60abd7fd6cd42bad08f54deb0d7f2dc31beb5
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2023-04-20 17:22:13 +0100

    updated

---
+++ src/pages/Press/PressData.js
@@ -0,0 +1,14 @@
+export const pressData = [
+  {
+    id: 1,
+    title: "Short description",
+    description:
+      "Umba is a customer-centric digital bank that increases access to financial tools while eliminating predatory fees and minimums, empowering customers across Africa to control their financial lives by ensuring affordable, efficient, and widely available financial services.",
+  },
+  {
+    id: 2,
+    title: "Boiler description",
+    description:
+      "Umba's mission is to be the largest African bank, offering bank accounts and other key banking services to our growing customer base. We want to make our users' lives easier to interact with banks and mobile networks and provide a transparent service with informed analytics which helps our users take control of their financial lives. Umba is backed by leading venture capitalists, including Costanoa Ventures, Palm Drive Capital, Ludlow Ventures and Lux Capital.",
+  },
+]
